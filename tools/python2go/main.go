bc06c7c0d47c7906a0f392cc4ab4e57d485d9633
Andre Lehmann <aisberg@posteo.de>
2022-07-29 16:08:59 +0200

    feat: put converted files directly into pkg dir

--- tools/python2go/main.go
+++ tools/python2go/main.go
@@ -25,12 +25,6 @@ func main() {
 		die("No files found\n")
 	}
 
-	// create output dir
-	err := os.MkdirAll("go", 0755)
-	if err != nil {
-		die("Could not create output dir: %s", err)
-	}
-
 	// go through all files and convert them
 	for _, path := range files {
 		filename := strings.TrimSuffix(path, ".py")
@@ -55,7 +49,7 @@ func main() {
 				continue
 			}
 			line = strings.ReplaceAll(line, "#", "//")
-			line = strings.ReplaceAll(line, "None", `"\u0000"`)
+			line = strings.ReplaceAll(line, "None", `"\uffff"`)
 			// replace single quotes with double quotes
 			line = singleQuotePattern.ReplaceAllStringFunc(line, replaceSingleQuote)
 			content = append(content, line)
@@ -72,7 +66,7 @@ func main() {
 		}
 
 		// write to file
-		outpath := fmt.Sprintf("go/%s.go", filename)
+		outpath := fmt.Sprintf("../../pkg/unidecode/table/%s.go", filename)
 		outfile, err := os.OpenFile(outpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 		if err != nil {
 			die("Failed to create file %s: %s", outpath, err)
